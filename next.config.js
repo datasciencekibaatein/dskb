5d4cef0bfedffecdfc82c66b7f972f6d0cc1cd06
Dhruv <dhruv@Dhruvs-MacBook-Air.local>
2026-03-15 13:30:09 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ const nextConfig = {
     domains: ["img.youtube.com", "i.ytimg.com"],
     formats: ["image/avif", "image/webp"],
   },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
-  },
+  //compiler: {
+  //  removeConsole: process.env.NODE_ENV === "production",
+  //},
   allowedDevOrigins: ["dskb-dhruv.loca.lt"]
 };
 
